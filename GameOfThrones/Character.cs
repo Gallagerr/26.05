c0f2c01686099d0a5f295a916aa1ae26ebc1667b
sadiev <sadiev@itstep.kz>
2019-05-20 21:13:39 +0600

    Добавьте файлы проекта.

---
+++ GameOfThrones/Character.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfThrones
+{
+   public  class Character
+    {  
+      [JsonProperty("_id")]
+      public string Id { get; set; }
+
+      [JsonProperty("name")]
+      public string Name { get; set; }
+  }
+}
---
+++ GameOfThrones/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameOfThrones
+{
+  /// <summary>
+  /// Логика взаимодействия для MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    private Person person;
+    private ObservableCollection<Character> characters = new ObservableCollection<Character>();
+
+    public MainWindow()
+    {
+      InitializeComponent();
+      Map.Source = new Uri("https://2gis.ru/game_of_thrones?queryState=center%2F64.645615%2C17.368702%2Fzoom%2F14");
+    }
+
+
+  
+
+    private void listbox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+    {
+      Window1 window = new Window1();
+      window.Show();
+    }
+  }
+}
